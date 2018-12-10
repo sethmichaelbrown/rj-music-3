1880a7f733e863691db0b8e3cb7681007b57c5ee
SMB <sethmichaelbrown@gmail.com>
2018-12-10 13:35:29 -0700

    Mobile layout corrections

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', (event) => {
   console.log("It's Alive")
   M.AutoInit();
 
+  fab = document.getElementById("fab-menu");
 
-fab = document.getElementById("fab-menu");
-
-var fabScroll = () => {
-  let y = window.scrollY;
-  console.log(y)
-  if (y >= 10) {
-    fab.setAttribute('class', 'fixed-action-btn')
+  const fabScroll = () => {
+    let y = window.scrollY
+    if (y >= 10) {
+      fab.setAttribute('class', 'fixed-action-btn')
+    }
+    else{
+      fab.setAttribute('class', 'hide')
+    }
   }
-  else{
-    fab.setAttribute('class', 'hide')
-  }
-}
 
-window.addEventListener("scroll", fabScroll);
+  window.addEventListener("scroll", fabScroll)
 
 
 
-}) // DOMContentLoaded;
+}) // DOMContentLoaded
